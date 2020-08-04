9f7ebcc0b7aaad1451cda757e7578406461f1993
Iris Ferreira <iris.ferreira@petronect.com.br>
2020-08-03 23:51:41 -0300

    commits css

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,13 +1,42 @@
 import React from 'react';
 
 import logoImg from '../../assets/images/logo.svg';
+import ladingImg from '../../assets/images/landing.svg';
 
-function Landing(){
+import studyIcon from '../../assets/images/icons/study.svg';
+import givenClassesIcon from '../../assets/images/icons/give-classes.svg';
+import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+
+import './styles.css';
+
+function Landing() {
     return (
-        <div id="page-lading">
+        <div id="page-landing">
             <div id="page-lading-content" className="container">
                 <div id="logo-container">
-                    <img src={logoImg} alt="Proffy"/>
+                    <img src={logoImg} alt="Proffy" />
+                    <h2>Sua plataforma de estudos online.</h2>
+                </div>
+
+                <img src={ladingImg} alt="Plataforma de estudos" className="hero-image" />
+
+                <div className="buttons-container">
+                    <a href="" className="study">
+                        <img src={studyIcon} alt="Estudar" />
+                        Estudar
+
+                    </a>
+
+                    <a href="" className="study">
+                        <img src={givenClassesIcon} alt="Estudar" />
+                        Dar aulas
+
+                    </a>
+
+                    <span className="total-connections">
+                        Total de 200 conexões já realizadas 
+                        <img src={purpleHeartIcon} alt="Coração Roxo" />
+                    </span>
                 </div>
             </div>
         </div>
