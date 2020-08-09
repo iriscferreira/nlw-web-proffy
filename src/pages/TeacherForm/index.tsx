9bc168d15886fda149f0b88a35fdfbe7e20e0a9e
Iris Ferreira <iris.ferreira@petronect.com.br>
2020-08-09 16:16:46 -0300

    improve teacher form page

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -2,10 +2,45 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 
+import Input from '../../components/Input/index';
+
+import warningIcom from '../../assets/images/icons/warning.svg';
+
+import './styles.css';
+
 function TeacherForm() {
-    return(
+    return (
         <div id="page-teacher-form" className="container">
-            <PageHeader title="Que incrível que você quer dar aulas."/>
+            <PageHeader title="Que incrível que você quer dar aulas."
+                description="O primeiro passo é preencher esse formulário de inscrição"
+            />
+
+            <main>
+                <fieldset>
+                    <legend>Seus dados</legend>
+                    <Input name="subject" label="Nome Completo" />
+                    <Input name="avatar" label="Avatar" />
+                    <Input name="whatsApp" label="WhatsApp" />
+                </fieldset>
+
+                <fieldset>
+                    <legend>Sobre a aula</legend>
+                    <Input name="subject" label="Matéria" />
+                    <Input name="cost" label="Custo da sua hora por aula" />
+                </fieldset>
+
+                <footer>
+                    <p>
+                        <img src={warningIcom} alt="Aviso Importante" />
+                        Importante <br/>
+                        Preencha todos os dados.
+                    </p>
+                    <button type="button">
+                        Salvar cadastro
+                    </button>
+                </footer>
+
+            </main>
         </div>
     )
 }
